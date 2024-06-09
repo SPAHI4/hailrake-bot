0014f94c7c6a0cbe52cde0c5c41b50999ab7d18b
Anton S <spaxi4@yandex.ru>
2024-06-09 18:23:57 +0400

    heroku

--- app/bot.ts
+++ app/bot.ts
@@ -20,29 +20,37 @@ bot.use((ctx, next) => {
 bot.hears(RatingCommand.TRIGGERS, async (ctx) => {
   console.log('Received update in HEARS:', ctx.update);
 
-  await db.transaction(
-    async (tx) => {
-      if (!ctx.message.reply_to_message?.from) {
-        return;
-      }
+  await db
+    .transaction(
+      async (tx) => {
+        console.log('Transaction, message:', ctx.message);
 
-      const userRepository = new UserRepository(tx);
+        if (!ctx.message.reply_to_message?.from) {
+          return;
+        }
 
-      const ratingCommand = new RatingCommand(userRepository);
+        const userRepository = new UserRepository(tx);
 
-      const replyText = await ratingCommand.execute(
-        ctx.message.text,
-        ctx.from,
-        ctx.message.reply_to_message.from,
-        ctx.message.chat,
-      );
+        const ratingCommand = new RatingCommand(userRepository);
 
-      // @ts-expect-error reply_to_message_id actually exists
-      await ctx.replyWithHTML(replyText, { reply_to_message_id: ctx.message.message_id });
-    },
-    {
-      isolationLevel: 'repeatable read',
-      accessMode: 'read write',
-    },
-  );
+        const replyText = await ratingCommand.execute(
+          ctx.message.text,
+          ctx.from,
+          ctx.message.reply_to_message.from,
+          ctx.message.chat,
+        );
+
+        console.log('Reply:', replyText);
+
+        // @ts-expect-error reply_to_message_id actually exists
+        await ctx.replyWithHTML(replyText, { reply_to_message_id: ctx.message.message_id });
+      },
+      // {
+      //   isolationLevel: 'repeatable read',
+      //   accessMode: 'read write',
+      // },
+    )
+    .catch((error: unknown) => {
+      console.error('Error in transaction:', error);
+    });
 });
