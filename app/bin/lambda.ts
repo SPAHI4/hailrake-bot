e94d855f016e9aa35284fc349a77fd7b78f846b2
Anton S <spaxi4@yandex.ru>
2024-06-09 15:59:09 +0400

    start script

--- app/bin/lambda.ts
+++ app/bin/lambda.ts
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-argument */
 import net from 'node:net';
+import { setDefaultResultOrder } from 'node:dns';
 
 import { APIGatewayProxyHandler } from 'aws-lambda';
 
 import { bot } from '../bot.js';
 
+setDefaultResultOrder('ipv4first');
 net.setDefaultAutoSelectFamily(false);
 
 export const handler: APIGatewayProxyHandler = async (event) => {
